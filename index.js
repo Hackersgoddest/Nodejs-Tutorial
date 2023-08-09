4b23f58158a162da391a6ade6b2970ecf43e5687
Oppong Frank Konadu <frankoppongkonadu930@gmail.com>
2023-08-09 10:57:46 +0000

    simple http server

---
+++ index.js
@@ -0,0 +1,14 @@
+// simple HTTP server   
+
+const http = require('node:http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end("Hello World!");
+})
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log('Server is running on port 3000');
+})
